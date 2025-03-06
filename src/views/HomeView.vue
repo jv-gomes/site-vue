86415940e16fb97de3be5ab52eb0d88e2479a05d
João Vitor <jv155gomes@gmail.com>
2025-03-06 16:50:29 -0300

    feat: add charts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import {ref} from 'vue'
 
 const lineChartSeries = ref([
   {
-    name:'Sales',
+    name:'vendas',
     data:[30,40,35,60,76,10,45]
   }
 ])
@@ -14,6 +14,27 @@ const lineChartOptions = ref({
   }
 })
 
+const barChartSeries = ref([
+  {
+    name:'lucro',
+    data:[30,40,35,60,76,10,45]
+  }
+])
+
+const barChartOptions = ref({
+  xaxis:{
+    categories:['a','b','c','d','e','f','g']
+  }
+})
+
+const pieChartSeries = ref([
+30,40,35,60,76,10,45
+])
+
+const pieChartOptions = ref({
+    labels:['a','b','c','d','e','f','g']
+})
+
 </script>
 
 <template>
@@ -21,11 +42,41 @@ const lineChartOptions = ref({
     <div class="flex flex-col gap-16 p-24 items-center bg-gray-700">
       <h2>text </h2>
     <div class="w-1/3 bg-white rounded-xl p-4 " >
-    <h2 class="text-x1 font-semibold"> Teste de graficos</h2>
+    <h2 class="text-x1 font-semibold"> Grafico Linha</h2>
     <apexCharts 
       type="line"
       :series="lineChartSeries"
       :options="lineChartOptions"
+       />
+
+    </div>
+
+    <div class="w-1/3 bg-white rounded-xl p-4 " >
+    <h2 class="text-x1 font-semibold"> Grafico Barra</h2>
+    <apexCharts 
+      type="bar"
+      :series="barChartSeries"
+      :options="barChartOptions"
+       />
+
+    </div>
+
+    <div class="w-1/3 bg-white rounded-xl p-4 " >
+    <h2 class="text-x1 font-semibold"> Grafico Pizza</h2>
+    <apexCharts 
+      type="pie"
+      :series="pieChartSeries"
+      :options="pieChartOptions"
+       />
+
+    </div>
+
+    <div class="w-1/3 bg-white rounded-xl p-4 " >
+    <h2 class="text-x1 font-semibold"> Grafico Area</h2>
+    <apexCharts 
+      type="area"
+      :series="barChartSeries"
+      :options="barChartOptions"
        />
 
     </div>
