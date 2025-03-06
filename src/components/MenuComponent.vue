e53835dcec15f7782a0348ed9de3ae9f049f7573
João Vitor <jv155gomes@gmail.com>
2025-03-06 16:00:02 -0300

    init: start site-vue

---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </ul>
+</template>
+
+<style>
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+ul {
+    position: fixed;
+    top: 1rem;
+    left: 0;
+    right: 0;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+
+const lineChartSeries = ref([
+  {
+    name:'Sales',
+    data:[30,40,35,60,76,10,45]
+  }
+])
+
+const lineChartOptions = ref({
+  xaxis:{
+    categories:['a','b','c','d','e','f','g']
+  }
+})
+
+</script>
+
+<template>
+  <main>
+    <div class="flex flex-col gap-16 p-24 items-center bg-gray-700">
+      <h2>text </h2>
+    <div class="w-1/3 bg-white rounded-xl p-4 " >
+    <h2 class="text-x1 font-semibold"> Teste de graficos</h2>
+    <apexCharts 
+      type="line"
+      :series="lineChartSeries"
+      :options="lineChartOptions"
+       />
+
+    </div>
+  </div>
+  </main>
+</template>
+
+<style>
+</style>
